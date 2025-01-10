9560d948699e2f44a1e103762db864d258eb3205
olanipekun01 <oloniluaeyitayo@gmail.com>
2025-01-10 05:48:16 -0800

    l

--- static/js/staff.js
+++ static/js/staff.js
@@ -5,14 +5,16 @@ const staffMembers = [
     name: "Professor Ayodele Samuel ONAWUMI ",
     qualification: "B.Sc. M.Sc. Ph.D FNSE. FNIMechE MNISafetyE. COREN Regd",
     designation: "Dean of the College",
-    email: "@achievers.edu.ng",
-    image: "prof_ayodele_sa.png",
+    email: "@achievers.edu.ng, ",
+    phone: "+234 803 666 9825",
+    image: "prof_ayodele_sa.jpg",
   },
   {
     name: "Engr. Dr. Oluwole John FAMORIJI (Associate Professor)",
     qualification: "B.Tech. Electrical and Electronics Engineering (LAUTECH), M.Eng Communication Engineering (FUTA), PhD Electronic Science and Technology (USTC), MIEEE, MNSE, COREN Regd",
     designation: "SubDean of the College, and HOD (Department of Electrical and Information Engineering",
-    email: "@achievers.edu.ng",
+    email: "eie@achievers.edu.ng, famoriji.oj@achievers.edu.ng",
+    phone: "+234 816 441 9471",
     image: "engr_Oluwole_jf.png",
   },
   {
@@ -20,13 +22,15 @@ const staffMembers = [
     qualification: "B.Eng, M.Eng. PhD,  MNSE,  R. Engr., MMSN, FNIEM, MIAENG, MWASET, MAASCIT, FIMPD, FICISE",
     designation: "HOD (Department of Mechanical and Mechatronics Engineering)",
     email: "@achievers.edu.ng",
+    phone: "+234 803 373 7251",
     image: "engr_prof_kareem.png",
   },
   {
-    name: "Engr. Prof. Sesan Ppeter Ayodeji",
+    name: "Engr. Prof. Sesan Peter Ayodeji",
     qualification: "FNSE",
     designation: "HOD (Department of Biomedical Engineering)",
     email: "@achievers.edu.ng",
+    phone: "+234 803 670 9782",
     image: "prof_sesan_pa.jpg",
   },
   {
@@ -34,7 +38,8 @@ const staffMembers = [
     qualification: "B.Eng Civil Engineering (Ilorin), M.Eng Structural Engineering (Akure), PhD Engineering (Liverpool, UK), FNSE, FNICE, M.ASCE, COREN Regd",
     designation: "HOD (Department of Civil and Environmental Engineering)",
     email: "@achievers.edu.ng",
-    image: "prof_david_ao.jpg",
+    phone: "+234 803 439 8502",
+    image: "prof_david_ao.png",
   },
 ];
 
@@ -45,7 +50,7 @@ staffMembers.forEach((staff) => {
     <div class="overflow-hidden border rounded-lg">
       <div class="grid md:grid-cols-[200px_1fr] gap-6">
         <div class="bg-gray-100">
-          <img src="../assets/${staff.image}" alt="${staff.name}" class="w-full h-full object-cover">
+          <img src="/static/assets/${staff.image}" alt="${staff.name}" class="w-full h-full object-cover">
         </div>
         <div class="p-6">
           <div class="grid grid-cols-[120px_1fr] gap-4 items-baseline">
